e830616d6530646d3611e6491af67873a7ab4f83
Jay Wang <jmw86@duke.edu>
2015-02-23 00:57:09 -0500

    wrong loc for sql

--- application_workloads/weight/query/overweight_zips.sql
+++ application_workloads/weight/query/overweight_zips.sql
@@ -24,17 +24,19 @@ OVERWRITE into TABLE weight;
 
 
 
-DROP TABLE population_zip; 
+DROP TABLE population_density_zip; 
 
-CREATE TABLE IF NOT EXISTS population_zip (
+CREATE TABLE IF NOT EXISTS population_density_zip (
 	zip_code INT,
-	population INT)
+	population INT,
+	land_sq_mi DOUBLE,
+	pop_density_sq_mi DOUBLE)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
 STORED AS TEXTFILE 
-LOCATION '/Users/jaywang/Desktop/data/hive_tables/population_zip';
+LOCATION '/Users/jaywang/Desktop/data/hive_tables/population_density_zip';
 
-LOAD DATA LOCAL INPATH '/Users/jaywang/Desktop/data/input_data/population_zip.csv' 
-OVERWRITE into TABLE population_zip;
+LOAD DATA LOCAL INPATH '/Users/jaywang/Desktop/data/input_data/population_density_zip.csv' 
+OVERWRITE into TABLE population_density_zip;
 
 
 
@@ -43,15 +45,15 @@ DROP TABLE top_overweight_zips;
 
 CREATE TABLE IF NOT EXISTS top_overweight_zips (
 	zip_code INT,
-	population INT,
+	pop_density_sq_mi DOUBLE,
 	percent_overweight DOUBLE)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
 STORED AS TEXTFILE 
 LOCATION '/Users/jaywang/Desktop/data/hive_tables/top_overweight_zips';
 
 INSERT OVERWRITE TABLE top_overweight_zips
-	SELECT p.zip_code, p.population AS population, w.percent_overweight_obese 
-	FROM weight w JOIN population_zip p ON w.zip_code = p.zip_code
+	SELECT p.zip_code, p.pop_density_sq_mi AS pop_density, w.percent_overweight_obese 
+	FROM weight w JOIN population_density_zip p ON w.zip_code = p.zip_code
 	ORDER BY w.percent_overweight_obese DESC
 	LIMIT 100;
 
@@ -62,7 +64,7 @@ DROP TABLE least_overweight_zips;
 
 CREATE TABLE IF NOT EXISTS least_overweight_zips (
 	zip_code INT,
-	population INT,
+	pop_density_sq_mi DOUBLE,
 	percent_overweight DOUBLE)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
 STORED AS TEXTFILE 
@@ -70,8 +72,8 @@ LOCATION '/Users/jaywang/Desktop/data/hive_tables/least_overweight_zips';
 
 
 INSERT OVERWRITE TABLE least_overweight_zips
-	SELECT p.zip_code, p.population, w.percent_overweight_obese 
-	FROM weight w JOIN population_zip p ON w.zip_code = p.zip_code
+	SELECT p.zip_code, p.pop_density_sq_mi AS pop_density, w.percent_overweight_obese 
+	FROM weight w JOIN population_density_zip p ON w.zip_code = p.zip_code
 	WHERE w.percent_overweight_obese IS NOT NULL 
 	ORDER BY w.percent_overweight_obese ASC
 	LIMIT 100;
@@ -79,11 +81,11 @@ INSERT OVERWRITE TABLE least_overweight_zips
 
 
 
-SELECT 'Average population per zip code: ', AVG(population) AS avg_pop 
-FROM population_zip;
+SELECT 'Average population density per zip code: ', AVG(pop_density_sq_mi) AS avg_pop_density_sq_mi 
+FROM population_density_zip;
 
-SELECT 'Average population per zip code (top overweight): ', AVG(population) AS avg_pop 
+SELECT 'Average population density per zip code (top overweight): ', AVG(pop_density_sq_mi) AS avg_pop_density_sq_mi 
 FROM top_overweight_zips;
 
-SELECT 'Average population per zip code (least overweight): ', AVG(population) AS avg_pop 
+SELECT 'Average population density per zip code (least overweight): ', AVG(pop_density_sq_mi) AS avg_pop_density_sq_mi 
 FROM least_overweight_zips;
